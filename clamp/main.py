0a3b50668fa509e1e44ae3d542ee51e8a9bb0f0d
cutplane1 <snakesnake222@mail.ru>
2025-07-28 23:38:37 +0500

    clamp: first version :)

---
+++ clamp/main.py
@@ -0,0 +1,57 @@
+from bottle import route, post, run, request
+from clamav_client.scanner import get_scanner # type: ignore
+import tempfile
+import os
+
+index_page = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>clamp</title>
+</head>
+<body>
+    <form action="/scan" method="post" enctype="multipart/form-data">
+        <input type="file" name="f">
+        <br>
+        <input type="submit" value="scan this file">
+    </form>
+</body>
+</html>"""
+
+result_page = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>{}</title>
+</head>
+<body style="background-color: {};">
+    <h1>{}</h1>
+</body>
+</html>"""
+
+scanner = get_scanner()
+
+@route('/')
+def index():
+    return index_page
+
+@post('/scan')
+def scan():
+    f = request.files.get('f')
+
+    if f is None and not f.filename:
+        return "No file uploaded"
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        path = os.path.join(tmpdir, f.filename)
+        f.save(path)
+        scan_res = scanner.scan(path)
+        if scan_res.state == "ERROR":
+            return "Internal error"
+        elif scan_res.state == "OK":
+            return result_page.format("PASS", "white", "FILE IS OK")
+        elif scan_res.state == "FOUND":
+            return result_page.format(scan_res.details, "red", scan_res.details)
+        return ":3"
+
+
+
+run(host='localhost', port=8080)
--- wallpaint/main.py
+++ wallpaint/main.py
@@ -1,9 +1,39 @@
-# plan:
+from PIL import Image, ImageDraw, ImageFont
+
 """
-drawing on transparent rect on image
-everything else
+ _____  ___  ___  ___   
+/__   \/___\/   \/___\_ 
+  / /\//  // /\ //  /(_)
+ / / / \_// /_// \_// _ 
+ \/  \___/___,'\___/ (_)
+
+* automatic text measure
+* nice font rendering
+* setting os wallpaper
+* image resizing
+* nicer api
+* rewriting to something portable (like C)
+* tty formating
+* something else
 """
-cal = """
+
+class Wallpaint:
+    def __init__(self, image, font):
+        self.wp = image
+        self.font = font
+        self.fl = Image.new("RGBA", image.size, (0, 0, 0, 0))
+        self.ctx = ImageDraw.Draw(self.fl)
+    
+    def rect(self,x,y,w,h,fill):
+        self.ctx.rectangle([(x,y),(x+w,y+h)], fill = fill, width=0)
+    
+    def out(self):
+        o = Image.alpha_composite(self.wp, self.fl).convert("RGB")
+        return o
+
+def CalendarWidget(wp, x, y):
+    # temp :)
+    cal = """
       July 2025
 Su Mo Tu We Th Fr Sa
        1  2  3  4  5
@@ -13,29 +43,37 @@ Su Mo Tu We Th Fr Sa
 27 28 29 30 31
 """
 
-# print(cal)
+    wp.rect(x, y, 250, 200, (0,0,0,128))
+    #ikr, font rendering sucks :{
+    wp.ctx.fontmode="1"
+    wp.ctx.text((x + 10, y + 10), cal, font=wp.font, fill=(255, 255, 255, 128))
 
-from PIL import Image, ImageDraw, ImageFont
+def NoteWidget(wp, x, y, notes):
+    wp.rect(x, y, 250, 200, (0,0,0,128))
+    wp.ctx.fontmode="1"
+    for i, note in enumerate(notes):
+        wp.ctx.text((x + 10 , y + 10 + (i*20)), "* " + note, font=wp.font, fill=(255, 255, 255, 128))
 
-font = ImageFont.truetype("consola.ttf", 20)
+def WeatherWidget(wp,x,y):
+    aaa = R"""
+Weather report: Hell
 
-wallpaper_src = "wallhaven-rq75r7_1920x1080.png"
-wallpaper = Image.open(wallpaper_src)
+     \  /       Partly cloudy
+   _ /"".-.     20 °C          
+     \_(   ).   ↘ 4 km/h       
+     /(___(__)  10 km          
+                0.1 mm                                  
+"""
+    wp.rect(x, y, 520, 200, (0,0,0,128))
+    #ikr, font rendering sucks :{
+    wp.ctx.fontmode="1"
+    wp.ctx.text((x + 10, y + 10), aaa, font=wp.font, fill=(255, 255, 255, 128))
+
+wp = Wallpaint(Image.open("wallhaven-rq75r7_1920x1080.png"), ImageFont.truetype("consola.ttf", 20))
 
 
-fl = Image.new("RGBA", wallpaper.size, (0, 0, 0, 0))
+CalendarWidget(wp, 300, 300)
+NoteWidget(wp, 600,600, ["eat cake", "sleep", "go outside"])
+WeatherWidget(wp, 1000, 300)
 
-ctx = ImageDraw.Draw(fl)
-
-
-def rect(ctx,x,y,w,h,fill):
-    ctx.rectangle([(x,y),(x+w,y+h)], fill = fill, width=0)
-
-rect(ctx, 300, 300, 250, 200, (0,0,0,128))
-
-#ikr, font rendering sucks :{
-ctx.fontmode="1"
-ctx.text((310, 310), cal, font=font, fill=(255, 255, 255, 128))
-
-out = Image.alpha_composite(wallpaper, fl).convert("RGB")
-out.show()
+wp.out().save("out.png")
