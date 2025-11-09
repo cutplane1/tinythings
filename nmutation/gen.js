2afb8089cf74b132b371d10c22df91810efed241
cutplane1 <snakesnake222@mail.ru>
2025-11-09 23:03:59 +0500

    Reapply ":4"
    This reverts commit 22da5328c9e8e700bc0f2495714432ebe6700dc5.

--- nmutation/gen.js
+++ nmutation/gen.js
@@ -2,17 +2,45 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function last_divisor(n) {
+    for (let i = n - 1; i >= 1; i--) {
+        if (n % i === 0) {
+            return i;
+        }
+    }
+    return 1;
+}
+
 function mutate(n) {
     let a = getRandomInt(n) + 1;
 
-    switch (getRandomInt(2)) {
+    switch (getRandomInt(4)) {
         case 0: return `(${a} + ${n - a})`;
-        case 1: return `(${a+n} - ${a})`;
+        case 1: return `(${a + n} - ${a})`;
+        case 2: {
+            if (n > 999) {
+                return mutate(n);
+            }
+            if (a === 0) {
+                return `(${n} * 1)`;
+            } else {
+                return `(${n * a} / ${a})`;
+            }
+        }
+        case 3: {
+            if (n > 999) {
+                return mutate(n);
+            }
+            let d = last_divisor(n);
+            if (d === n) {
+                d = 1;
+            }
+            return `(${n / d} * ${d})`;
+        }
     }
 }
 
-function walk(buf, skip_chance = 0.3) {
-
+function walk(buf, skip_chance = 0.2) {
     return buf.replace(/\d+/g, (match) => {
         if (Math.random() < skip_chance) {
             return match;
